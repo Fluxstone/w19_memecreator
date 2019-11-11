d75f7eadd0de4309375b70f12bb3e68adce7f01d
Yannick Borschneck <yannick-borschneck@outlook.de>
2019-11-11 21:21:15 +0100

    Develop: Erster Demo-Testversuch zur Demonstration unseres Kontexfensters.

--- w19_memecreator/MainWindow.xaml.cs
+++ w19_memecreator/MainWindow.xaml.cs
@@ -24,6 +24,39 @@ namespace w19_memecreator
         {
             InitializeComponent();
         }
-        
+
+        Button btn_DEBUG_Button1 = new Button();
+
+        public void set_btn_DEBUG_Button1_properties()
+        {
+            btn_DEBUG_Button1.Height = 25;
+            btn_DEBUG_Button1.Width = 80;
+            btn_DEBUG_Button1.Content = "Codebutton";
+
+            btn_DEBUG_Button1.HorizontalAlignment = HorizontalAlignment.Left;
+            btn_DEBUG_Button1.VerticalAlignment = VerticalAlignment.Top;
+            btn_DEBUG_Button1.Margin = new Thickness(700, 200, 0, 0);
+
+            btn_DEBUG_Button1.AddHandler(Button.ClickEvent, new RoutedEventHandler(btn_DEBUG_Button1_Click));
+        }
+       
+
+        private void btn_DEBUG_Control1_Click(object sender, RoutedEventArgs e)
+        {
+            set_btn_DEBUG_Button1_properties();
+            grid_mainGrid.Children.Add(btn_DEBUG_Button1);
+        }
+
+        private void btn_DEBUG_Control2_Click(object sender, RoutedEventArgs e)
+        {
+            grid_mainGrid.Children.Remove(btn_DEBUG_Button1);
+        }
+
+        private void btn_DEBUG_Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ok");
+        }
+
+
     }
 }
