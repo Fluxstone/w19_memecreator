eaee1055bb108e9499f74ed84649ec32a17580bb
Dominique Bauer <dominique.s.bauer@gmail.com>
2019-11-24 19:03:27 +0100

    Added example template json, parsing for template previews

--- w19_memecreator/MainWindow.xaml.cs
+++ w19_memecreator/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace w19_memecreator
 {
@@ -23,7 +16,38 @@ namespace w19_memecreator
         public MainWindow()
         {
             InitializeComponent();
+
+            using (StreamReader r = new StreamReader(Environment.CurrentDirectory + "\\..\\..\\Templates\\templates.json"))
+            {
+                string json = r.ReadToEnd();
+                dynamic templates = JsonConvert.DeserializeObject(json);
+
+                foreach (var template in templates)
+                {
+                    Image myImage = new Image();
+                    myImage.Width = 200;
+
+                    // Create source
+                    BitmapImage myBitmapImage = new BitmapImage();
+
+                    // BitmapImage.UriSource must be in a BeginInit/EndInit block
+                    myBitmapImage.BeginInit();
+                    myBitmapImage.UriSource = new Uri(Environment.CurrentDirectory + "\\..\\..\\Templates\\Previews\\" + template.previewSource, UriKind.Absolute);
+
+                    // To save significant application memory, set the DecodePixelWidth or  
+                    // DecodePixelHeight of the BitmapImage value of the image source to the desired 
+                    // height or width of the rendered image. If you don't do this, the application will 
+                    // cache the image as though it were rendered as its normal size rather then just 
+                    // the size that is displayed.
+                    // Note: In order to preserve aspect ratio, set DecodePixelWidth
+                    // or DecodePixelHeight but not both.
+                    myBitmapImage.DecodePixelHeight = 100;
+                    myBitmapImage.EndInit();
+                    //set image source
+                    myImage.Source = myBitmapImage;
+                    stackP_Timeline.Children.Add(myImage);
+                }
+            }
         }
-        
     }
 }
